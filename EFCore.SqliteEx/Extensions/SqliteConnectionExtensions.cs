6c000d436707c85d1863530a52dab5ab449347f0
Brice Lambson <bricelam@users.noreply.github.com>
2018-06-04 21:35:12 -0700

    Add spatial dependencies

--- EFCore.SqliteEx/Extensions/SqliteConnectionExtensions.cs
+++ EFCore.SqliteEx/Extensions/SqliteConnectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.Data.Sqlite
 {
@@ -6,10 +7,27 @@ namespace Microsoft.Data.Sqlite
     {
         public static SqliteConnection AddEFCoreExtensions(this SqliteConnection connection)
         {
+            // TimeSpan
             connection.CreateFunction("days", (TimeSpan value) => value.TotalDays);
             connection.CreateFunction("timespan", (double value) => TimeSpan.FromDays(value));
 
+            // NetTopologySuite
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT load_extension('mod_spatialite');";
+
+                // Let the CLR load the library from NuGet package assets
+                var temp = spatialite_version();
+
+                connection.EnableExtensions();
+                command.ExecuteNonQuery();
+                connection.EnableExtensions(false);
+            }
+
             return connection;
         }
+
+        [DllImport("mod_spatialite")]
+        private static extern IntPtr spatialite_version();
     }
 }
